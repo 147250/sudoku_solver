6e6b596717a72ad66be0ace22abc560f76e6a622
None <None>
2021-11-29 18:03:26 +0400

    add class SudokuSolver and function check_num

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
+from copy import deepcopy
+
+
+def check_num(matrix: list, num: int, x: int, y: int):
+    if num in matrix[x]:
+        return False
+    for elem in matrix:
+        if elem[y] == num:
+            return False
+    return True
+
+
+class SudokuSolver:
+
+    def __init__(self, matrix):
+        self.matrix = deepcopy(matrix)
+        self.empty_cell_cords = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if not matrix[i][j]:
+                    self.empty_cell_cords.append((i, j))
+
+
 if __name__ == '__main__':
-    pass
+    w = [[i % 2 for i in range(4)] for i in range(4)]
+    q = SudokuSolver(w)
