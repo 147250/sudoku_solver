1ad9988f2992b65423c9cd3b8d655fd670318cdb
None <None>
2021-12-07 18:03:43 +0400

    add method run in SudokuSolver rework check_num

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from copy import deepcopy
 
 
-def check_num(matrix: list, num: int, x: int, y: int):
-    if num in matrix[x]:
-        return False
-    for elem in matrix:
-        if elem[y] == num:
-            return False
-    return True
+def check_num(matrix: list, num: int, row: int, column: int):
+
+    horizontal = matrix[row]
+    vertical = [elem[column] for elem in matrix]
+    if horizontal.count(num) == vertical.count(num) == 0:
+        return True
+
+    return False
 
 
 class SudokuSolver:
@@ -20,7 +21,25 @@ class SudokuSolver:
                 if not matrix[i][j]:
                     self.empty_cell_cords.append((i, j))
 
+    def run(self):
+        pos = 0
+        while -1 < pos < len(self.empty_cell_cords):
+            i, j = self.empty_cell_cords[pos]
+            num = self.matrix[i][j] + 1
+            self.matrix[i][j] = 0
+            check = check_num(self.matrix, num, i, j)
 
-if __name__ == '__main__':
-    w = [[i % 2 for i in range(4)] for i in range(4)]
-    q = SudokuSolver(w)
+            while num < 10 and not check:
+                num += 1
+                check = check_num(self.matrix, num, i, j)
+
+            if num < 10:
+                self.matrix[i][j] = num
+                pos += 1
+            else:
+                pos -= 1
+
+        if pos == len(self.empty_cell_cords):
+            return self.matrix
+        else:
+            return f"No solution"
